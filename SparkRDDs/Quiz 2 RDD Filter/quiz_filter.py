13e9e98754de7684d7c92fb9041c9db4d12ed3e6
Alec Moldovan <alec_moldovan@yahoo.com>
2023-03-03 17:13:51 -0600

    Refactored quiz_filter.py. All logic inside lambda and chained methods.

--- SparkRDDs/Quiz 2 RDD Filter/quiz_filter.py
+++ SparkRDDs/Quiz 2 RDD Filter/quiz_filter.py
@@ -7,13 +7,6 @@ sc = SparkContext.getOrCreate(conf=conf)
 rdd = sc.textFile("sample.txt")
 
 
-def filter_func(word):
-    if word.startswith("a") or word.startswith("c"):
-        return False
-    else:
-        return True
-
-
 rdd2 = rdd.flatMap(lambda x: x.split(" ")).filter(
     lambda x: not (x.startswith("a") or x.startswith("c")))
 
