4f0cb515f2cc2b5731d65fca2ce0447f8ef087b4
Zalasyu <alec_moldovan@yahoo.com>
2023-03-21 12:53:36 -0500

    Created my own schema.

--- SparkDFs/SchemaDFs/provideSchema.py
+++ SparkDFs/SchemaDFs/provideSchema.py
@@ -1,4 +1,16 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+
+schema = StructType([
+    StructField("age", IntegerType(), True),
+    StructField("gender", StringType(), True),
+    StructField("name", StringType(), True),
+    StructField("course", StringType(), True),
+    StructField("roll", StringType(), True),
+    StructField("marks", IntegerType(), True),
+    StructField("email", StringType(), True),
+])
+
 
 spark = SparkSession.builder.appName("provideSchema").getOrCreate()
 
